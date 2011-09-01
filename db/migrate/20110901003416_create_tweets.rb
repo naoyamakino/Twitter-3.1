8bbc3f613c8f93d518cb48769a2d98aa9525f882
naoyamakino <naoyamakino@gmail.com>
2011-08-31 17:37:25 -0700

    fix and migrate

--- db/migrate/20110901003416_create_tweets.rb
+++ db/migrate/20110901003416_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
       t.string :body
-      t.int :user_id
+      t.integer :user_id
 
       t.timestamps
     end
